f15ae04fdbbc5d69ea1bde73050c6396e6f28fc7
Jonathan Jethmal <jonathan.jethmal1513@gmail.com>
2026-03-16 12:19:25 -0500

    definitive fix

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,8 +1,8 @@
 const User = require("./User");
 const Patient = require("./Patient");
-const { BPReading } = require("./BPReading");
-const { GlucoseReading } = require("./GlucoseReading");
-const { WeightReading } = require("./WeightReading");
+const BPReading = require("./BPReading");
+const GlucoseReading = require("./GlucoseReading");
+const WeightReading = require("./WeightReading");
 const { Medication, MedLog } = require("./Medication");
 const Screening = require("./Screening");
 const { TCCProgress, ABCRecord, SMARTGoal, HungerScale } = require("./TCC");
